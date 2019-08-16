5dff10bb16039529b73fc6fca0f825ca2311b4f3
SupriyaVarada <53267194+SupriyaVarada@users.noreply.github.com>
2019-08-16 16:10:21 +0530

    Changes made for Task 317-Generate AWF-Copy Description and Workflow

--- ValIAAttrsOfECRAndAddECRToAWFRelationTab/src/com/agile/px/ValIAAttrsOfECRAndAddECRToAWFRelationTab.java
+++ ValIAAttrsOfECRAndAddECRToAWFRelationTab/src/com/agile/px/ValIAAttrsOfECRAndAddECRToAWFRelationTab.java
@@ -10,14 +10,14 @@ import com.agile.api.IAgileSession;
 import com.agile.api.IChange;
 import com.agile.api.INode;
 import com.agile.api.ITable;
-import com.agile.util.CommonUtil;
+import com.agile.util.GenericUtilities;
 
 /**
  * 
- * @author Supriya Varada
- * This PX displays a exception message to user if any Impact assessment attributes are not filled on eCR during
- *  Change Status for workflow Pre event 
- * This PX adds the eCR to the relationship tab of AWF during  Change Status for workflow Post event 
+ * @author Supriya
+ * Pre Event: This PX displays a exception message to user if any Impact assessment attributes are not filled on eCR during
+ *  Status change from Pending to review 
+ * Post Event: This PX adds the eCR to the relationship tab of AWF during status change from Pending to Review
  * 
  *
  */
@@ -34,104 +34,86 @@ public class ValIAAttrsOfECRAndAddECRToAWFRelationTab implements IEventAction {
 	public EventActionResult doAction(IAgileSession session, INode arg1, IEventInfo eventInfo) {
 
 		try {
-			//Initialize logger
-			CommonUtil.initAppLogger(ValIAAttrsOfECRAndAddECRToAWFRelationTab.class, session);
+			// Initialize logger
+			GenericUtilities.initializeLogger(session);
 
 			HashMap<Object, Object> genericMessagesList = new HashMap<Object, Object>();
 			HashMap<Object, Object> attributesMappingOnECRAndAWFList = new HashMap<Object, Object>();
 			HashMap<Object, Object> awfMessagesList = new HashMap<Object, Object>();
 
-			if (eventInfo.getEventType() == EventConstants.EVENT_CHANGE_STATUS_FOR_WORKFLOW) {
+			IWFChangeStatusEventInfo info = (IWFChangeStatusEventInfo) eventInfo;
 
-				IWFChangeStatusEventInfo info = (IWFChangeStatusEventInfo) eventInfo;
+			// Get Agile List Values
+			genericMessagesList = GenericUtilities.getAgileListValues(session, genericMessagesListName);
+			attributesMappingOnECRAndAWFList = GenericUtilities.getAgileListValues(session,
+					attributesMappingOnECRAndAWFListName);
+			awfMessagesList = GenericUtilities.getAgileListValues(session, awfMessagesListName);
 
-				// Load List Values
-				genericMessagesList = CommonUtil.loadListValues(session, genericMessagesListName);
-				attributesMappingOnECRAndAWFList = CommonUtil.loadListValues(session,
-						attributesMappingOnECRAndAWFListName);
-				awfMessagesList = CommonUtil.loadListValues(session, awfMessagesListName);
+			// Get AWF Object Number
+			IChange AWF = (IChange) info.getDataObject();
+			logger.debug("AWF Object is" + AWF);
 
-				// Get AWF Object Number
-				IChange AWF = (IChange) info.getDataObject();
-				logger.debug("AWF Object is" + AWF);
+			if (AWF != null) {
 
-				if (AWF != null) {
+				// Get ECR Object on AWF
+				String strECR = GenericUtilities.getSingleListAttributeValue(AWF,
+						awfMessagesList.get("ECR_ATTRID").toString());
+				logger.debug("ECR is:" + strECR);
+				IChange eCR = null;
+				if (strECR != null) {
+					eCR = (IChange) session.getObject(IChange.OBJECT_TYPE, strECR);
+					logger.debug("ECR is:" + eCR);
+				}
 
-					// Get ECR Object on AWF
-					String strECR = CommonUtil.getSingleListValue(AWF, awfMessagesList.get("ECR_ATTRID").toString());
-					logger.debug("ECR is:" + strECR);
-					IChange eCR = null;
-					if (strECR != null) {
-						eCR = (IChange) session.getObject(IChange.OBJECT_TYPE, strECR);
-						logger.debug("ECR is:" + eCR);
-					}
+				if (eCR != null) {
 
-					if (eCR != null) {
+					if (eventInfo.getEventTriggerType() == EventConstants.EVENT_TRIGGER_PRE) {
 
-						if (eventInfo.getEventTriggerType() == EventConstants.EVENT_TRIGGER_PRE) {
+						// Get eCR Impact assessment attribute Values
+						HashMap<Object, Object> eCRAttrValues = new HashMap<Object, Object>();
+						eCRAttrValues = GenericUtilities.getIAAttributeValues(eCR, attributesMappingOnECRAndAWFList);
+						logger.debug("eCRAttrValues are:" + eCRAttrValues);
 
-							HashMap<Object, Object> eCRAttrValues = new HashMap<Object, Object>();
-							String attrValue = "";
+						int countOfEmptyAttrs = 0;
+						countOfEmptyAttrs = GenericUtilities.getCountOfIAAttributes(
+								genericMessagesList.get("NULL").toString(), eCRAttrValues, session);
+						logger.debug("count of empty Attributes is" + countOfEmptyAttrs);
 
-							//Get eCR attribute Ids
-							for (Object key : attributesMappingOnECRAndAWFList.keySet()) {
-								if (attributesMappingOnECRAndAWFList.get(key) != null) {
-									attrValue = CommonUtil.getSingleListValue(eCR, key.toString());
-									eCRAttrValues.put(key, attrValue);
-								}
-							}
-							logger.debug("eCRAttrValues are:" + eCRAttrValues);
-
-							int countOfEmptyAttrs = 0;
-							String value = null;
-
-							for (Object key : eCRAttrValues.keySet()) {
-								value = (String) eCRAttrValues.get(key);
-								if (value == null || value.equals("")) {
-										countOfEmptyAttrs++;
-								}
-							}
-
-							logger.debug("count of empty Attributes is" + countOfEmptyAttrs);
-							if (countOfEmptyAttrs > 0) {
-								actionResult = new ActionResult(ActionResult.EXCEPTION, new Exception(
-										awfMessagesList.get("ECRVALUES_NOT_FILLED1").toString() + " " + eCR.toString()+"."
-										+awfMessagesList.get("ECRVALUES_NOT_FILLED2").toString()));
-							}  else {
-								actionResult = new ActionResult(ActionResult.STRING,
-										genericMessagesList.get("VALIDATION_SUCCESS").toString());
-							}
-						}else if(eventInfo.getEventTriggerType() == EventConstants.EVENT_TRIGGER_POST) {
-							// Add ECR to relationship tab of AWF
-							ITable relationshipTab = AWF.getTable(ChangeConstants.TABLE_RELATIONSHIPS);
-							@SuppressWarnings("unchecked")
-							Iterator<IChange> it = relationshipTab.getReferentIterator();
-							IChange change = null;
-							HashSet<IChange> relationObjects = new HashSet<IChange>();
-							while(it.hasNext()) {
-								change = (IChange) it.next();
-								relationObjects.add(change);
-								
-							}
-							if(relationObjects.contains(eCR)) {
-								actionResult = new ActionResult(ActionResult.STRING,awfMessagesList.get("ECR_ALREADY_ADDED_TO_AWF").toString());
-								
-							}else {
-								relationshipTab.createRow(eCR);
-								actionResult = new ActionResult(ActionResult.STRING,awfMessagesList.get("ECR_ADDED_TO_AWF").toString());
-							}
+						if (countOfEmptyAttrs > 0) {
+							actionResult = new ActionResult(ActionResult.EXCEPTION,
+									new Exception(awfMessagesList.get("ECRVALUES_NOT_FILLED1").toString() + " "
+											+ eCR.toString() + "."
+											+ awfMessagesList.get("ECRVALUES_NOT_FILLED2").toString()));
+						} else {
+							actionResult = new ActionResult(ActionResult.STRING,
+									genericMessagesList.get("VALIDATION_SUCCESS").toString());
 						}
-
 					} else {
-						actionResult = new ActionResult(ActionResult.STRING,
-								genericMessagesList.get("INVALID_EVENT_TRIGGER_TYPE").toString());
+						// Add ECR to relationship tab of AWF
+						ITable relationshipTab = AWF.getTable(ChangeConstants.TABLE_RELATIONSHIPS);
+						@SuppressWarnings("unchecked")
+						Iterator<IChange> it = relationshipTab.getReferentIterator();
+						IChange change = null;
+						HashSet<IChange> relationObjects = new HashSet<IChange>();
+						while (it.hasNext()) {
+							change = (IChange) it.next();
+							relationObjects.add(change);
+
+						}
+						if (relationObjects.contains(eCR)) {
+							actionResult = new ActionResult(ActionResult.STRING,
+									awfMessagesList.get("ECR_ALREADY_ADDED_TO_AWF").toString());
+
+						} else {
+							relationshipTab.createRow(eCR);
+							actionResult = new ActionResult(ActionResult.STRING,
+									awfMessagesList.get("ECR_ADDED_TO_AWF").toString());
+						}
 					}
 
-				} else {
-					actionResult = new ActionResult(ActionResult.STRING,
-							genericMessagesList.get("INVALID_EVENT_TYPE").toString());
 				}
 			}
+
 		}
 
 		catch (APIException e) {
