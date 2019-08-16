91ffeda27e44b563d9245828921992a0fe0caedb
SupriyaVarada <53267194+SupriyaVarada@users.noreply.github.com>
2019-08-16 16:13:10 +0530

    Changes made for Task 317-Generate AWF-Copy Description and Workflow

--- UpdateChangeClassificationOnECR/src/com/agile/px/UpdateChangeClassificationOnECR.java
+++ UpdateChangeClassificationOnECR/src/com/agile/px/UpdateChangeClassificationOnECR.java
@@ -8,16 +8,16 @@ import com.agile.api.APIException;
 import com.agile.api.IAgileSession;
 import com.agile.api.IChange;
 import com.agile.api.INode;
-import com.agile.util.CommonUtil;
+import com.agile.util.GenericUtilities;
 
 /**
  * 
- * @author Supriya Varada
- * If any Impact assessment attribute is modified and if all the Impact assessment attributes are filled, 
- * this PX sets Change classification to 
- *  - Minor if all Impact assessment attributes are filled as No,
- *  - Major if any Impact assessment attribute is filled as Yes.
- *  
+ * @author Supriya
+ *  If any Impact assessment attribute is modified and
+ *   if all the impact assessment attributes are filled as No, this PX sets Change classification to - Minor
+ *   if any Impact assessment attributes is filled as Yes, this PX sets Change classification to -Major 
+ *   if any Impact assessment attribute is not filled, this PX sets Change classification to -NULL 
+ * 
  */
 
 public class UpdateChangeClassificationOnECR implements IEventAction {
@@ -32,118 +32,94 @@ public class UpdateChangeClassificationOnECR implements IEventAction {
 	@Override
 	public EventActionResult doAction(IAgileSession session, INode arg1, IEventInfo eventInfo) {
 		try {
-			//Initialize logger
-			CommonUtil.initAppLogger(UpdateChangeClassificationOnECR.class, session);
+			// Initialize logger
+			GenericUtilities.initializeLogger(session);
 
 			HashMap<Object, Object> genericMessagesList = new HashMap<Object, Object>();
 			HashMap<Object, Object> attributesMappingOnECRAndAWFList = new HashMap<Object, Object>();
 			HashMap<Object, Object> ecrMessagesList = new HashMap<Object, Object>();
 
-			// Load List Values
-			genericMessagesList = CommonUtil.loadListValues(session, genericMessagesListName);
-			attributesMappingOnECRAndAWFList = CommonUtil.loadListValues(session, attributesMappingOnECRAndAWFListName);
-			ecrMessagesList = CommonUtil.loadListValues(session, ecrMessagesListName);
+			// Get Agile List Values
+			genericMessagesList = GenericUtilities.getAgileListValues(session, genericMessagesListName);
+			attributesMappingOnECRAndAWFList = GenericUtilities.getAgileListValues(session,
+					attributesMappingOnECRAndAWFListName);
+			ecrMessagesList = GenericUtilities.getAgileListValues(session, ecrMessagesListName);
 
-			if (eventInfo.getEventType() == EventConstants.EVENT_UPDATE_TITLE_BLOCK) {
+			IUpdateTitleBlockEventInfo info = (IUpdateTitleBlockEventInfo) eventInfo;
 
-				IUpdateTitleBlockEventInfo info = (IUpdateTitleBlockEventInfo) eventInfo;
+			// Get eCR Object Number
+			IChange eCR = (IChange) info.getDataObject();
+			logger.debug("ECR Object is" + eCR);
 
-				// Get ECR Object Number
-				IChange eCR = (IChange) info.getDataObject();
-				logger.debug("ECR Object is" + eCR);
+			if (eCR != null) {
+				String result = "";
 
-				if (eCR != null) {
+				// Get Change Impact assessment attribute Ids and values
+				HashMap<Object, Object> assessmentAttrValues = new HashMap<Object, Object>();
+				assessmentAttrValues = GenericUtilities.getIAAttributeValues(eCR, attributesMappingOnECRAndAWFList);
+				logger.debug("Assessment Attribute Values are:" + assessmentAttrValues);
 
-					HashMap<Object, Object> assessmentAttrValues = new HashMap<Object, Object>();
-					String attrValue = "";
-					List<Integer> assessmentAttrIds = new ArrayList<Integer>();
+				// Get Assessment attribute IDs into arraylist
+				List<Integer> assessmentAttrIds = new ArrayList<Integer>();
+				for (Object key : assessmentAttrValues.keySet()) {
+					assessmentAttrIds.add(Integer.parseInt(key.toString()));
+				}
+				logger.debug("Assesment attribute ids are:" + assessmentAttrIds);
 
-					// Get Change Impact assessment attribute Ids and values
-					for (Object key : attributesMappingOnECRAndAWFList.keySet()) {
-						if (attributesMappingOnECRAndAWFList.get(key) != null) {
-							assessmentAttrIds.add(Integer.parseInt(key.toString()));
-							attrValue = CommonUtil.getSingleListValue(eCR, key.toString());
-							assessmentAttrValues.put(key, attrValue);
-						}
+				// Get Dirty attribute IDs
+				int i = 0;
+				IEventDirtyCell[] cells = info.getCells();
+				int dirtyAttrId = 0;
+				boolean flag = false;
+				while (i < cells.length) {
+					dirtyAttrId = cells[i].getAttributeId();
+					if (assessmentAttrIds.contains(dirtyAttrId)) {
+						flag = true;
+						break;
 					}
-					logger.debug("Assesment attribute ids are:" + assessmentAttrIds);
-					logger.debug("assessmentAttrValues are:" + assessmentAttrValues);
+					i++;
+				}
+				logger.debug("Flag value is:" + flag);
 
-					if (eventInfo.getEventTriggerType() == EventConstants.EVENT_TRIGGER_POST) {
-						String result = "";
+				// If any assessment attribute is modified,
+				// set Change Classification based on Impact Assessment attribute
+				// values
+				if (flag == true) {
+					// Calculate the number of attributes which are empty and has yes value.
+					int countOfEmptyAttrs = 0;
+					int countOfAttrsWithYes = 0;
 
-						// Get Dirty attribute IDs
-						int i = 0;
-						IEventDirtyCell[] cells = info.getCells();
-						int dirtyAttrId = 0;
-						boolean flag = false;
-						while (i < cells.length) {
-							dirtyAttrId = cells[i].getAttributeId();
-							if (assessmentAttrIds.contains(dirtyAttrId)) {
-								flag = true;
-								break;
-							}
-							i++;
-						}
-						logger.debug("Flag value is:" + flag);
-						
-						// If any assessment attribute is modified,
-						// set Change Classification based on Impact Assessment attribute
-						// values
-						if (flag == true) {
-							// Calculate the number of attributes which are empty and has yes value.
-							int countOfEmptyAttrs = 0;
-							int countOfAttrsWithYes = 0;
-							String value = "";
-							for (Object key : assessmentAttrValues.keySet()) {
-								value = (String) assessmentAttrValues.get(key);
-								if (value == null || value.equalsIgnoreCase("")) {
-									countOfEmptyAttrs++;
-								}
+					countOfAttrsWithYes = GenericUtilities.getCountOfIAAttributes(
+							genericMessagesList.get("YES").toString(), assessmentAttrValues, session);
+					countOfEmptyAttrs = GenericUtilities.getCountOfIAAttributes(
+							genericMessagesList.get("NULL").toString(), assessmentAttrValues, session);
 
-								if (value != null
-										&& value.equalsIgnoreCase(genericMessagesList.get("YES").toString())) {
-									countOfAttrsWithYes++;
-								}
+					logger.debug("count of empty Attributes is" + countOfEmptyAttrs);
+					logger.debug("count of yes Attributes is" + countOfAttrsWithYes);
 
-							}
-							logger.debug("count of empty Attributes is" + countOfEmptyAttrs);
-							logger.debug("count of yes Attributes is" + countOfAttrsWithYes);
-							if (countOfEmptyAttrs == 0) {
-								if (countOfAttrsWithYes >= 1) {
-									eCR.setValue(
-											Integer.parseInt(
-													ecrMessagesList.get("CHANGE_CLASSIFICATION_ATTRID").toString()),
-											ecrMessagesList.get("MAJOR_LIST_VALUE").toString());
-									result = result + ecrMessagesList.get("CLASSIFICATION_SET_TO_MAJOR").toString();
-								} else {
-									eCR.setValue(
-											Integer.parseInt(
-													ecrMessagesList.get("CHANGE_CLASSIFICATION_ATTRID").toString()),
-											ecrMessagesList.get("MINOR_LIST_VALUE").toString());
-									result = result + ecrMessagesList.get("CLASSIFICATION_SET_TO_MINOR").toString();
-								}
-							} else {
-								result = result + ecrMessagesList.get("CHG_CLASS_NOT_UPDATED").toString();
-							}
-
+					if (countOfEmptyAttrs != 0) {
+						eCR.setValue(Integer.parseInt(ecrMessagesList.get("CHANGE_CLASSIFICATION_ATTRID").toString()),
+								null);
+						result = result + ecrMessagesList.get("CLASSIFICATION_SET_TO_NULL").toString();
+					} else {
+						if (countOfAttrsWithYes >= 1) {
+							eCR.setValue(
+									Integer.parseInt(ecrMessagesList.get("CHANGE_CLASSIFICATION_ATTRID").toString()),
+									ecrMessagesList.get("MAJOR_LIST_VALUE").toString());
+							result = result + ecrMessagesList.get("CLASSIFICATION_SET_TO_MAJOR").toString();
 						} else {
-							result = result + ecrMessagesList.get("ASSMT_ATTRS_NOT_EDITED").toString();
+							eCR.setValue(
+									Integer.parseInt(ecrMessagesList.get("CHANGE_CLASSIFICATION_ATTRID").toString()),
+									ecrMessagesList.get("MINOR_LIST_VALUE").toString());
+							result = result + ecrMessagesList.get("CLASSIFICATION_SET_TO_MINOR").toString();
 						}
-						logger.debug("Result is:" + result);
-						actionResult = new ActionResult(ActionResult.STRING, result);
-
-					}
-
-					else {
-						actionResult = new ActionResult(ActionResult.STRING,
-								genericMessagesList.get("INVALID_EVENT_TRIGGER_TYPE").toString());
 					}
 
 				} else {
-					actionResult = new ActionResult(ActionResult.STRING,
-							genericMessagesList.get("INVALID_EVENT_TYPE").toString());
+					result = result + ecrMessagesList.get("ASSMT_ATTRS_NOT_EDITED").toString();
 				}
+				logger.debug("Result is:" + result);
+				actionResult = new ActionResult(ActionResult.STRING, result);
 
 			}
 		}
